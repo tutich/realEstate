9981f70c08d9f115c0b34ce0d08691c30de702d3
tutich <135505990+tutich@users.noreply.github.com>
2024-04-24 13:32:14 +0300

    about,contact, blog css

--- client/src/components/pages/Pages.jsx
+++ client/src/components/pages/Pages.jsx
@@ -5,10 +5,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "../home/Home"
 import Footer from "../common/footer/Footer"
 import About from "../about/About"
-// import Pricing from "../pricing/Pricing"
-// import Blog from "../blog/Blog"
-// import Services from "../services/Services"
-// import Contact from "../contact/Contact"
+import Blog from "../blog/Blog"
+import Services from "../services/Services"
+import Pricing from "../pricing/Pricing"
+import Contact from "../contact/Contact"
 
 const Pages = () => {
   return (
@@ -18,10 +18,10 @@ const Pages = () => {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
-          {/* <Route exact path='/services' component={Services} />
-          <Route exact path='/blog' component={Blog} />
-          <Route exact path='/pricing' component={Pricing} />
-          <Route exact path='/contact' component={Contact} /> */}
+          <Route path='/blog' element={<Blog />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/pricing' element={<Pricing />} />
+          <Route path='/contact' element={<Contact />} />
         </Routes>
         <Footer />
       </Router>
