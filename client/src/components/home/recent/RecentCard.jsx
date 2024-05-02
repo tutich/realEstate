9f6dddb1454f57b3df52e186d95aad54fd8e32d2
tutich <135505990+tutich@users.noreply.github.com>
2024-05-02 10:17:42 +0300

    featured filter

--- client/src/components/home/recent/RecentCard.jsx
+++ client/src/components/home/recent/RecentCard.jsx
@@ -1,40 +1,68 @@
-
-import React from "react"
-import { list } from "../../data/Data"
+import React, { useState } from "react";
+import { list } from "../../data/Data";
 
 const RecentCard = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredList = list.filter(
+    (item) =>
+      item.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      item.location.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <>
-      <div className='content grid3 mtop'>
-        {list.map((val, index) => {
-          const { cover, category, location, name, price, type } = val
+       <div className="search-box">
+          <input
+            type="text"
+            placeholder="Search by name or ..."
+            value={searchTerm}
+            onChange={handleSearch}
+          />
+        </div>
+      <div className="content grid3 mtop">
+        {filteredList.map((val, index) => {
+          const { cover, category, location, name, price, type } = val;
           return (
-            <div className='box shadow' key={index}>
-              <div className='img'>
-                <img src={cover} alt='' />
+            <div className="box shadow" key={index}>
+              <div className="img">
+                <img src={cover} alt="" />
               </div>
-              <div className='text'>
-                <div className='category flex'>
-                  <span style={{ background: category === "For Sale" ? "#25b5791a" : "#ff98001a", color: category === "For Sale" ? "#25b579" : "#ff9800" }}>{category}</span>
-                  <i className='fa fa-heart'></i>
+              <div className="text">
+                <div className="category flex">
+                  <span
+                    style={{
+                      background:
+                        category === "For Sale" ? "#25b5791a" : "#ff98001a",
+                      color: category === "For Sale" ? "#25b579" : "#ff9800",
+                    }}
+                  >
+                    {category}
+                  </span>
+                  <i className="fa fa-heart"></i>
                 </div>
                 <h4>{name}</h4>
                 <p>
-                  <i className='fa fa-location-dot'></i> {location}
+                  <i className="fa fa-location-dot"></i> {location}
                 </p>
               </div>
-              <div className='button flex'>
+              <div className="button flex">
                 <div>
-                  <button className='btn2'>{price}</button> <label htmlFor=''>/sqft</label>
+                  <button className="btn2">{price}</button>{" "}
+                  <label htmlFor="">/sqft</label>
                 </div>
                 <span>{type}</span>
               </div>
             </div>
-          )
+          );
         })}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default RecentCard
+export default RecentCard;
